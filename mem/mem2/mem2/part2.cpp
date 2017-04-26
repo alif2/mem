045d446ccc1401fb3e21e2aaa5d96d77c1ef8a70
Adam Lif <adamlnk@neb.rr.com>
2017-04-26 00:36:39 -0500

    Part 2 init

--- mem/mem2/mem2/part2.cpp
+++ mem/mem2/mem2/part2.cpp
@@ -6,7 +6,7 @@
 #include <algorithm>
 
 const long PAGE_SIZE = 256;
-const long PAGE_COUNT = 256;
+const long PAGE_COUNT = 128;
 const long TLB_SIZE = 16;
 
 using namespace std;
@@ -21,12 +21,15 @@ struct page {
 	int pagenum;
 	char data[PAGE_SIZE];
 	bool modified = false;
+	int created;
+	int used;
 };
 
 struct tlb_page {
 	int pagenum;
 	int index;
 	bool modified = false;
+	int used;
 };
 
 string get_bitset(bitset<32> bit, int start, int length) {
@@ -38,7 +41,7 @@ string get_bitset(bitset<32> bit, int start, int length) {
 }
 
 int page_hit(page page[], unsigned long pagen) {
-	for (unsigned int i = 0; i < PAGE_SIZE; i++) {
+	for (unsigned int i = 0; i < PAGE_COUNT; i++) {
 		if (long(page[i].pagenum) == pagen) return i;
 	}
 	return -1;
@@ -51,8 +54,62 @@ int tlb_hit(tlb_page tlb[], int pagen) {
 	return -1;
 }
 
+int get_victim_tlb(tlb_page tlb[]) {
+	int min = INT32_MAX;
+	int index = -1;
+	for (unsigned int i = 0; i < TLB_SIZE; i++) {
+		if (tlb[i].used < min) {
+			min = tlb[i].used;
+			index = i;
+		}
+	}
+
+	if (min == INT32_MAX) return -1;
+
+	return index;
+}
+
+int get_victim_frame(page page[], string strategy)
+{
+	int index = -1;
+	if(strategy.compare("fifo") == 0)
+	{
+		int min = INT32_MAX;
+		for(unsigned int i = 0; i < PAGE_COUNT; i++)
+		{
+			if (page[i].created < min) {
+				min = page[i].created;
+				index = i;
+			}
+		}
+
+		if (min == INT32_MAX) return -1;
+
+		return index;
+	}
+
+	if (strategy.compare("lru") == 0)
+	{
+		int min = INT32_MAX;
+		for(unsigned int i = 0; i < PAGE_COUNT; i++)
+		{
+			if (page[i].used < min) {
+				min = page[i].used;
+				index = i;
+			}
+		}
+
+		if (min == INT32_MAX) return -1;
+
+		return index;
+	}
+	
+	cout << "Unknown strategy\n";
+	return -1;
+}
+
 int main(int argc, char **argv) {
-	if (argc < 3) {
+	if (argc < 4) {
 		cout << "Not enough arguments\n";
 		exit(1);
 	}
@@ -98,54 +155,83 @@ int main(int argc, char **argv) {
 	int faults = 0, tlbhits = 0;
 	page pages[PAGE_COUNT];
 	tlb_page tlb[TLB_SIZE];
-	bool tlbfull = false;
 	for (unsigned int i = 0; i < addresses.size(); i++) {
 		int physadd = 0, val = 0, hit;
 
+		// TLB check
 		int tlbhit = tlb_hit(tlb, addresses.at(i).page);
 		if (tlbhit >= 0) {
 			physadd = tlbhit * PAGE_SIZE + addresses.at(i).offset;
 			val = pages[tlbhit].data[addresses.at(i).offset];
+			pages[tlbhit].used = i;
 			tlbhits++;
 		}
 
+		// TLB miss, Page table check
 		else if ((hit = page_hit(pages, addresses.at(i).page)) >= 0) {
 			physadd = hit * PAGE_SIZE + addresses.at(i).offset;
 			val = pages[hit].data[addresses.at(i).offset];
+			pages[hit].used = i;
 		}
 
+		// Page fault
 		else {
 			struct page newpage;
-
 			memory.seekg(addresses.at(i).page * PAGE_SIZE, ios::beg);
 			memory.read(newpage.data, PAGE_SIZE);
 
 			newpage.pagenum = addresses.at(i).page;
-			for (unsigned int j = 0; j < PAGE_COUNT; j++) {
-				if (!pages[j].modified) {
-					pages[j] = newpage;
-					pages[j].modified = true;
-					physadd = j * PAGE_SIZE + addresses.at(i).offset;
-					val = newpage.data[addresses.at(i).offset];
+			newpage.created = i;
+			newpage.used = i;
 
-					if (!tlbfull) {
+			// Page table initial scheduling
+			if (faults < PAGE_COUNT) {
+				for (unsigned int j = 0; j < PAGE_COUNT; j++) {
+					if (!pages[j].modified) {
+						pages[j] = newpage;
+						pages[j].modified = true;
+						physadd = j * PAGE_SIZE + addresses.at(i).offset;
+						val = newpage.data[addresses.at(i).offset];
+
+						// TLB scheduling
+						bool found = false;
 						for (unsigned int k = 0; k < TLB_SIZE; k++) {
 							if (!tlb[k].modified) {
 								tlb[k].index = j;
 								tlb[k].pagenum = newpage.pagenum;
 								tlb[k].modified = true;
+								tlb[k].used = i;
 
-								if (k >= TLB_SIZE - 1) tlbfull = true;
+								found = true;
 								break;
 							}
 						}
-					}
+						
+						// If the TLB is already full, replace existing
+						if(!found) {
+							int replace = get_victim_tlb(tlb);
+							tlb[replace].index = j;
+							tlb[replace].pagenum = newpage.pagenum;
+							tlb[replace].modified = true;
+							tlb[replace].used = i;
+						}
 
-					break;
+						break;
+					}
 				}
+				faults++;
 			}
-			faults++;
-		}	
+
+			// After page table is full, need replacement strategy
+			else
+			{
+				int replace = get_victim_frame(pages, argv[3]);
+				pages[replace] = newpage;
+				pages[replace].modified = true;
+				physadd = replace * PAGE_SIZE + addresses.at(i).offset;
+				val = newpage.data[addresses.at(i).offset];
+			}
+		}
 		correct << "Virtual address: " << addresses.at(i).virt << " Physical address: " << physadd << " Value: " << val << "\n";
 	}
 
@@ -157,6 +243,6 @@ int main(int argc, char **argv) {
 	correct << "TLB Hits = " << tlbhits << "\n";
 	correct << "TLB Hit Rate = " << double(tlbhits) / addresses.size() << "\n";
 	correct << "\n";
-	
+
 	correct.close();
 }
